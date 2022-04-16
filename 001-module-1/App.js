8b19baf3a1c07bb2c6c226113fc461fae077eae9
Marco Guidara <marco.guidara@efesto.studio>
2022-04-16 21:53:00 +0200

    Lesson 16

--- 001-module-1/App.js
+++ 001-module-1/App.js
@@ -1,26 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummyText}>Another piece of text</Text>
+        <TextInput placeholder="Your course goal!"/>
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Hello world</Text>
-      <Button title="tap me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dummyText: {
-    margin: 16, borderWidth: 2, borderColor: 'blue', padding: 16
+  appContainer: {
+    padding: 50
   }
 });
