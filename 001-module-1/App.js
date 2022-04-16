b0a7312540821a3d5154d0775379b7d55ff9ee32
Marco Guidara <marco.guidara@efesto.studio>
2022-04-16 23:01:57 +0200

    Lesson 22

--- 001-module-1/App.js
+++ 001-module-1/App.js
@@ -3,14 +3,18 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { useState } from "react";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
   }
 
   return (
@@ -24,7 +28,7 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        { courseGoals.map(goal => <Text key={goal}>goal</Text>)}
       </View>
     </View>
   );
